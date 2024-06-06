581da29373e73e9a42481ce91e9dc0890723b080
Ochuba Ekene Alexander <147551927+OchubaEkene@users.noreply.github.com>
2024-06-06 15:54:57 +0100

    Update account.py
    I added the student account

--- account.py
+++ account.py
@@ -46,3 +46,23 @@ class SavingsAccount(Account):
 
 d = SavingsAccount()
 d.deposit(100000000)
+
+
+from account import Account
+
+
+class StudentAccount(Account):
+    def deposit(self, amount):
+        if amount <= 50000:
+            super().deposit(amount)
+        else:
+            print("You have reached your deposit limit")
+
+    def withdraw(self, amount):
+        if amount <= 2000:
+            super().withdraw(amount)
+        else:
+            print("You have reached your withdrawal limit")
+
+d = StudentAccount()
+d.withdraw(20000)
