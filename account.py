2297abff9f8c3d241dee3848ee062f897666ad3e
Lucky-shedrack <150241168+Lucky-shedrack@users.noreply.github.com>
2024-06-06 12:19:32 +0100

    Update account.py

--- account.py
+++ account.py
@@ -11,5 +11,17 @@ class Account:
         self.balance -= amount
         print('Account balance is: ', self.balance)
 
+class ChildrenAccount(Account):
+    def deposit(self, amount):
+        self.balance += amount * 0.007
+        super().deposit(amount)
+
+
+    def withdraw(self, amount):
+        print("No withdrawals are allowed for children account")
+
+d = ChildrenAccount()
+d.deposit(1000)
+d.withdraw(10)
 
 
